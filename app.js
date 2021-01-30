9d18b765cb82fab3b95d6210259c5378867a532f
Ahmet Uzgor <uzgor.a@justsnap.de>
2021-01-30 15:24:54 +0300

    mongodb on root is deleted

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const logger = require('morgan');
 const dotEnv = require('dotenv').config();
 const verifyToken = require('./services/jwtHelper');
 
-// Connect to Mongo DB
-const mongo = require('./config/database')
-mongo.connectToDB();
+// // Connect to Mongo DB
+// const mongo = require('./config/database')
+// mongo.connectToDB();
 
 // Add routers to app
 const usersRouter = require('./routes/users');
