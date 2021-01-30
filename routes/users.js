a22e5a57195f56573e08e48549fc6b3afdd77c3c
Ahmet Uzgor <uzgor.a@justsnap.de>
2021-01-30 14:10:51 +0300

    small bugfix is completed

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs'); // Password hashing
-const jwt = require('jsonwebtoken');
-const bodyParser = require('body-parser');
 const { authenticate } = require('../controllers/userController');
 const { createUser, findByUsername } = require('../services/userListDB');
-
-//urlencodedParser
-const urlencodedParser = express().use(bodyParser.urlencoded({extended: false}))
 
 // users/ GET user register or login page 
 router.get('/', (req, res)=> {
@@ -16,15 +11,17 @@ router.get('/', (req, res)=> {
 
 // users/register POST. it creates a new user with specifed username & password
 router.post('/register', (req,res)=>{
+  // res.json({message: "User created"});
   const {username , password} = req.body;
   if (!username || !password) res.json({ message: "Username or password is not specified" });
+  if(typeof password !== 'string') password = `${password}`;
   bcrypt.hash(password,10).then((hash)=>{
 
     // it creeates user on userList array which is like small Db to keep all users infos and passwords with hash  
-    const userCreated = createUser(username, hash).then(user => {
-      if(!user) res.json({ message: 'User not found' })
+    createUser(username, hash).then(user => {
+      if(!user) res.json({ message: 'User not created' })
       else {
-        res.json({ status:'OK', response:'User created' })
+        res.json({ status:'OK', message:'User created' })
       }
     }).catch(err => console.log(err))
 
@@ -34,9 +31,8 @@ router.post('/register', (req,res)=>{
 });
 
 // users/authenticate POST user with its name & password and JWT creates and compare of user info's
-router.post('/authenticate', urlencodedParser, (req,res)=>{
+router.post('/authenticate', (req,res)=>{
   const {username , password} = req.body;
-  const api_secret_key =  process.env.API_KEY
   
   if (!username || !password) res.json({ message: 'Username or password is null' })
   const user = findByUsername(username)
