f2848bd40eb290c697a35b0f842b4695cd9d36a6
Ahmet Uzgor <uzgor.a@justsnap.de>
2021-01-31 20:22:17 +0300

    small if error is fixed

--- routes/trips.js
+++ routes/trips.js
@@ -9,23 +9,21 @@ client.connect().then((mongoDB)=> { // it starts mongoDb connection to find nece
     // it gets all trips which are started in a region specified by a point and time(optional) 
     router.post('/all', async (req, res) => {
         const { Point , radius, start_date, end_date } = req.body;
-        if (!Point || !Point.long || Point.lat || !radius) res.json({message: 'Required parameters are missing, Point & radius'}) // if Point or radius is not given, it returns message
+        if (!Point || !Point.long || !Point.lat || !radius) res.json({message: 'Required parameters are missing, Point & radius'}) // if Point or radius is not given, it returns message
         if (typeof Point.long !== 'number' || typeof Point.lat !== 'number' || typeof radius !== 'number') res.json({ message : 'Parameters are not in intended formats, all should be number'})
 
         const query = queries(Point, radius).all; // it keeps query for listing all trips by specified a point 
         if (start_date && new Date(start_date).toString() !== 'Invalid Date') query['start_date'] = new Date(start_date); // if start_date or end_date is specified, it adds to query for date
         if (end_date && new Date(end_date).toString() !== 'Invalid Date') query['complete_date'] = new Date(end_date);
 
-        const tripsList = []; // it initializes tripList as array to keep query result as array
         // it made a query for matched collection and saved to tripsList and respond as json object with data 
         tripCollection.find(query).toArray().then(data => res.json({ list: data }))
-        //tripCollection.find(query).forEach(data => tripsList.push(data.distance_travelled)).finally((data) => { res.json({list: tripsList}) })
     })
 
     // it gets the minimum and maximum distance travelled for the trips which are specified by a Point
     router.post('/getDistance', (req, res) => {
         const { Point , radius } = req.body;
-        if (!Point || !Point.long || Point.lat || !radius) res.json({message: 'Required parameters are missing, Point & radius'})
+        if (!Point || !Point.long || !Point.lat || !radius) res.json({message: 'Required parameters are missing, Point & radius'})
         if (typeof Point.long !== 'number' || typeof Point.lat !== 'number' || typeof radius !== 'number') res.json({ message : 'Parameters are not in intended formats, all should be number'})
 
         const query = queries(Point, radius).getDistance;
@@ -37,13 +35,13 @@ client.connect().then((mongoDB)=> { // it starts mongoDb connection to find nece
     // it gets a report which should include the number of trips grouped by vehicle model year for the trips 
     router.post('/getReport', (req, res) => {
         const { Point , radius } = req.body;
-        if (!Point || !Point.long || Point.lat || !radius) res.json({message: 'Required parameters are missing, Point & radius'})
+        if (!Point || !Point.long || !Point.lat || !radius) res.json({message: 'Required parameters are missing, Point & radius'})
         if (typeof Point.long !== 'number' || typeof Point.lat !== 'number' || typeof radius !== 'number') res.json({ message : 'Parameters are not in intended formats, all should be number'})
 
         const query = queries(Point, radius).getReport;
         
         tripCollection.aggregate(query).toArray().then(data => res.json({ list: data })).catch(err => res.json({ err: err, message: 'Query error'}))
     })
-}).catch(err => console.log('Connection Error'));
+}).catch(err => console.log('Connection Error', err));
 
 module.exports = router;
